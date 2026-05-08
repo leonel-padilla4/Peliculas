78ce97fb4f579f52ff6b2b9f9c421a8d27b349ef
leonel-padilla4 <yo18122022@gmail.com>
2026-05-08 13:03:17 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.tsx'
-import { HashRouter } from 'react-router-dom' 
+import { HashRouter } from 'react-router-dom'  // ← Única línea cambiada
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
+  <HashRouter>
     <App />
-    </BrowserRouter>
-    
-  </StrictMode>,
+  </HashRouter>
 )
